731d4813579b01ca42d252c2bfc04d9bc0687eed
Hugh Barclay <hugh@zencontrol.com>
2021-06-21 12:48:34 +1000

    Run npm install when generating certificates

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 
 FILE=./certs/cert.pem
 HOST_IP=`getent hosts host.docker.internal | awk '{ print $1 }'`
+
 if [ -f $FILE ]; then
    echo "Using existing certificate"
 else
+  (cd node-red && npm install)
   docker run -v ${PWD}/tls/generate-certs:/usr/local/bin/generate-certs -v ${PWD}/certs:/certs -e SSL_SIZE=4096 -e SSL_IP=${HOST_IP} paulczar/omgwtfssl
   cp ./certs/ca.pem ./node-red/certs/ca.pem
   sed -i -e 's/host.docker.internal/'$HOST_IP'/g' ./node-red/flows.json
